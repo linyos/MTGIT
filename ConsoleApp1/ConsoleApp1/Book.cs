8cae58440ab8ce957c0524d3056bbb4016b377f4
linyos <laso85143@gmail.com>
2023-02-19 21:24:57 +0800

    add book.cs

---
+++ ConsoleApp1/ConsoleApp1/Book.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    class Book
+    {
+        public string Title { get; set; }
+        public List<string> Authors { get; set; }
+        public DateTime PubliccationDate { get; set; }
+
+    }
+}
