b8518ab110644ef906397b853b56bcea26cb67cf
linyos <laso85143@gmail.com>
2023-02-19 21:16:47 +0800

    add calcuator

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -8,12 +8,23 @@ namespace ConsoleApp1
         {
             Console.WriteLine("Hello World!");
             Console.WriteLine("hihihi");
+
+
+            var result = Calcuator(10, 2);
+            Console.WriteLine(result);
+
         }
 
 
         static void Feature1()
         {
             Console.WriteLine("Feature1");
+        }
+
+
+        static int Calcuator(int a , int b)
+        {
+            return a + b;
         }
     }
 }
