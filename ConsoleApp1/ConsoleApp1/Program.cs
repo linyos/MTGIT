7c05636d5db0703fc78996ba098bc72a9e5512d5
linyos <laso85143@gmail.com>
2023-02-19 21:30:09 +0800

    change

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -10,21 +10,6 @@ namespace ConsoleApp1
             Console.WriteLine("hihihi");
 
 
-            var result = Calcuator(10, 2);
-            Console.WriteLine(result);
-
-        }
-
-
-        static void Feature1()
-        {
-            Console.WriteLine("Feature1");
-        }
-
-
-        static int Calcuator(int a , int b)
-        {
-            return a + b;
         }
     }
 }
